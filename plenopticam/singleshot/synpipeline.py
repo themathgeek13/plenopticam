e79fb98e19325fe1135f7d503eaa5d780fb2c3a9
rohan <rgrao@andrew.cmu.edu>
2020-11-26 22:49:40 -0500

    added image/depth fusion and redesigned/reimplemented the pipelines

--- plenopticam/singleshot/synpipeline.py
+++ plenopticam/singleshot/synpipeline.py
@@ -22,14 +22,32 @@ __license__ = """
 
 import numpy as np
 
+def generate_digital_zoom(inputimg, inputdepth, zoom_factor):
+    x1A, y1A, __ = np.indices(inputimg.shape)
+    H, W, C = inputimg.shape
+
+    u0 = [H//2, W//2]
+
+    x1B = zoom_factor*x1A + (1-zoom_factor)*u0[0]
+    x1B = x1B.astype(np.int16).transpose(2,0,1)
+    y1B = zoom_factor*y1A + (1-zoom_factor)*u0[1]
+    y1B = y1B.astype(np.int16).transpose(2,0,1)
+
+    zoomimg = np.zeros_like(inputimg)
+    zoomdepth = np.zeros_like(inputimg)
+    for i in range(C):
+        zoomimg[x1B[i],y1B[i],i] = inputimg[:, :, i]
+        zoomdepth[x1B[i],y1B[i],i] = inputdepth[:, :, i]
+
+    return zoomimg, zoomdepth
+
 class SynthesisPipeline(object):
     
     def __init__(self, *args, **kwargs):
         self.D0 = kwargs['D0'] if 'D0' in kwargs else 0.5
         self.t  = kwargs['t'] if 't' in kwargs else 0.2
-        self.D1A = kwargs['D1A'] if 'D1A' in kwargs else 0.9
         self.u0 = kwargs['u0'] if 'u0' in kwargs else None
-        self.zoom_factor = kwargs['zoom_factor'] if 'zoom_factor' in kwargs else 0.8
+        self.maskf = 1000
 
     def generate_synthesized_views(self, inputimg, inputdepth):
         x1A, y1A, __ = np.indices(inputimg.shape)
@@ -37,46 +55,40 @@ class SynthesisPipeline(object):
         if self.u0 is None:
             self.u0 = [H//2, W//2]
 
+        self.D1A = inputdepth
         x1B = self.D1A*(self.D0-self.t)*x1A/(self.D0*(self.D1A-self.t)) + self.t*(self.D1A-self.D0)*self.u0[0]/(self.D0*(self.D1A-self.t))
         x1B = x1B.astype(np.uint16).transpose(2,0,1)
         y1B = self.D1A*(self.D0-self.t)*y1A/(self.D0*(self.D1A-self.t)) + self.t*(self.D1A-self.D0)*self.u0[1]/(self.D0*(self.D1A-self.t))
         y1B = y1B.astype(np.uint16).transpose(2,0,1)
 
-        synthimg = np.zeros_like(inputimg)
-        synthdepth = np.zeros_like(inputdepth)
+        synthimg = np.ones_like(inputimg)*self.maskf
+        synthdepth = np.ones_like(inputdepth)*self.maskf
         for i in range(C):
-            synthimg[:,:,i] = inputimg[x1B[i], y1B[i], i]
-            synthdepth[:,:,i] = inputdepth[x1B[i], y1B[i], i]
+            synthimg[x1B[i],y1B[i],i] = inputimg[:, :, i]
+            synthdepth[x1B[i],y1B[i],i] = inputdepth[:, :, i]
 
         return synthimg, synthdepth
 
-    def generate_digital_zoom(self, inputimg, inputdepth):
-        x1A, y1A, __ = np.indices(inputimg.shape)
-        H, W, C = inputimg.shape
-
-        if self.u0 is None:
-            self.u0 = [H//2, W//2]
-
-        x1B = self.zoom_factor*x1A + (1-self.zoom_factor)*self.u0[0]
-        x1B = x1B.astype(np.uint16).transpose(2,0,1)
-        y1B = self.zoom_factor*y1A + (1-self.zoom_factor)*self.u0[1]
-        y1B = y1B.astype(np.uint16).transpose(2,0,1)
-
-        zoomimg = np.zeros_like(inputimg)
-        zoomdepth = np.zeros_like(inputimg)
-        for i in range(C):
-            zoomimg[:,:,i] = inputimg[x1B[i], y1B[i], i]
-            zoomdepth[:,:,i] = inputdepth[x1B[i], y1B[i], i]
-
-        return zoomimg, zoomdepth
-
 if __name__ == '__main__':
     I = np.load("/home/rohan/PycharmProjects/plenopticam/data/allfocus.npy")
-    D = np.load("/home/rohan/PycharmProjects/plenopticam/data/depthmap.npy")
+    D = np.load("/home/rohan/PycharmProjects/plenopticam/data/depthmap.npy")+1.0
     H, W, C = I.shape
-    kwargs = {'D0': 0.3, 't': 0.2, 'D1A': 0.9, 'u0': [2*H//3, W//2], 'zoom_factor': 0.9}
-    sp = SynthesisPipeline(**kwargs)
-    I1, D1 = sp.generate_digital_zoom(I, D)
+
+    # create the digitally zoomed versions I1 and I2
+    I1, D1 = generate_digital_zoom(I, D, zoom_factor=0.95)
     I2, D2 = I.copy(), D.copy()
-    I1DZ, D1DZ = sp.generate_synthesized_views(I1, D1)
-    I2DZ, D2DZ = sp.generate_synthesized_views(I2, D2)
+
+    # create two separate view synthesis pipelines
+    kwargs1 = {'D0': 1, 't': 0, 'u0': [H//2, W//2]}
+    sp1 = SynthesisPipeline(**kwargs1)
+    I1DZ, D1DZ = sp1.generate_synthesized_views(I1, D1)
+
+    kwargs2 = {'D0': 1, 't': 0.1, 'u0': [H//2, W//2]}
+    sp2 = SynthesisPipeline(**kwargs2)
+    I2DZ, D2DZ = sp2.generate_synthesized_views(I2, D2)
+
+    # image/depth fusion step
+    mask = np.zeros_like(I1DZ)
+    mask[np.where(I1DZ == sp1.maskf)] = 1
+    I_F = mask*I2DZ + (1-mask)*I1DZ
+    D_F = mask*D2DZ + (1-mask)*D1DZ
